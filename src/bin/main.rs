406fbb86d18496bf55f6afc5764d1f8150006158
sytse <git@sreitsma.nl>
2022-11-18 08:42:11 +0100

    Post to remote

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,35 @@
+use reqwest;
+use reqwest::header::CONTENT_TYPE;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::time::Duration;
 use hgi80_decoder::{parse_packet, Payload};
+use std::collections::HashMap;
+
+fn post_temperature_data(endpoint: &str, data: &HashMap<u8, f32>) {
+    let mut payload: String = String::from("[");
+    for (key, value) in data {
+        if payload.len() > 1 {
+            payload += ",";    
+        }
+        payload += &format!("{{\"id\":\"RADIATOR{}\", \"temp\":{} }}", key, value);
+    }
+    payload += "]";
+    println!("PAYLOAD {}", &payload);
+
+    let client = reqwest::blocking::Client::new();
+    let res = client.post(endpoint)
+        .header(CONTENT_TYPE, "application/json")
+        .body(payload)
+        .send();
+
+    if let Err(e) = res {
+        eprintln!("Failed to post data to {} ({})", endpoint, e);
+    }
+    else {
+        println!("Response: {}", res.unwrap().text().unwrap_or_default());
+    }
+}
 
 fn main() {
     let serial_port = serialport::new("/dev/ttyUSB0", 115200)
@@ -17,20 +45,20 @@ fn main() {
                 match parse_packet(&line.as_str()) {
                     Ok(packet) => {
                         if let Some(Payload::ZoneTemp(zt)) = packet.payload {
+                            post_temperature_data("http://xxxx", &zt.temperatures);
                             println!("Temperature {:?}", zt.temperatures);
                         }
                     }
                     Err(e) => {
-                        eprintln!("Error parsing line ({})", e);
+                        eprintln!("Error parsing line ({:#})", e);
                         eprintln!("  With line '{}'", line);
                     }
                 }
             }
             Err(e) => {
                 // Most likely a USB disconnect, restart
-                panic!("Error reading from serial port ({})", e);
+                panic!("Error reading from serial port ({:#})", e);
             }
         }
     }
-
 }

f985fbfe028ede61b282464ae9fde6b434a75105
Sytse Reitsma <git@sreitsma.nl>
2022-12-31 10:06:11 +0100

    More wip. HGI80 in thread

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,12 +1,5 @@
 use clap::Parser;
-use hgi80_decoder::{parse_packet, Payload};
-use reqwest::header::CONTENT_TYPE;
-use std::collections::HashMap;
-use std::io::BufRead;
-use std::io::BufReader;
-use std::time::Duration;
-use std::fs::OpenOptions;
-use std::io::prelude::*;
+use hgi80_decoder::hgi80::HGI80;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -21,79 +14,8 @@ struct Args {
     usb: String,
 }
 
-fn post_temperature_data(endpoint: &str, data: &HashMap<u8, f32>) {
-    let mut payload: String = String::from("[");
-    for (key, value) in data {
-        if payload.len() > 1 {
-            payload += ",";
-        }
-        payload += &format!("{{\"id\":\"RADIATOR{}\", \"temp\":{} }}", key, value);
-    }
-    payload += "]";
-    println!("PAYLOAD {}", &payload);
-
-    let client = reqwest::blocking::Client::new();
-    let res = client
-        .post(endpoint)
-        .header(CONTENT_TYPE, "application/json")
-        .body(payload)
-        .send();
-
-    if let Err(e) = res {
-        eprintln!("Failed to post data to {} ({})", endpoint, e);
-    } else {
-        println!("Response: {}", res.unwrap().text().unwrap_or_default());
-    }
-}
-
-fn evohome_loop(port: &str, endpoint: &str)
-{
-    let mut reader = BufReader::new(serial_port);
-    loop {
-        let mut line = String::new();
-        match reader.read_line(&mut line) {
-            Ok(_bytes) => match parse_packet(line.as_str()) {
-                Ok(packet) => {
-                    // Packets with an RSSI higher than 80 usually have lots of bit errors
-                    if packet.rssi < 80 { 
-                        if let Some(Payload::ZoneTemp(zt)) = packet.payload {
-                            post_temperature_data(&args.endpoint, &zt.temperatures);
-
-                            println!("Temperature {:?}", zt.temperatures);
-                            if let Err(e) = writeln!(file, "{}", line.as_str()) {
-                                eprintln!("Couldn't write to file: {}", e);
-                            }
-                        }
-                    }
-                }
-                Err(_e) => {
-                    //eprintln!("Error parsing line ({:#})", e);
-                    //eprintln!("  With line '{}'", line);
-                }
-            },
-            Err(e) => {
-                match e.kind() {
-                    std::io::ErrorKind::TimedOut => eprintln!("Error reading from serial port ({:#})", e),
-                    _ => panic!("Error reading from serial port ({:#})", e),
-                }
-            }
-        }
-    }
-}
-
 fn main() {
     let args = Args::parse();
-    
-    let mut file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .append(true)
-        .open("evohome_temps.txt")
-        .unwrap();
 
-    let serial_port = serialport::new(&args.usb, 115200)
-        .timeout(Duration::from_millis(2000))
-        .open()
-        .expect("Failed to open port");
-
+    let evohome = HGI80::new(&args.usb, &args.endpoint);
 }
